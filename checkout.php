055cfe6d9b2d88eec1168d55ee54e17b863634a8
ptang3 <ptang3@uci.edu>
2020-05-08 02:05:05 -0700

    Created sql tables in sql file on discord. Added php/mysql insert statements in checkout.php

--- checkout.php
+++ checkout.php
@@ -1,3 +1,46 @@
+<?php
+    require('db_conn.php');
+
+    //CUSTOMER INFO
+    if( isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['phone']) &&
+        isset($_POST['country']) && isset($_POST['streetAddress']) && isset($_POST['city']) &&
+        isset($_POST['zipcode']) && isset($_POST['shipping']) && isset($_POST['email'])) {
+
+        $sql = "INSERT INTO customers (firstName, lastName, phone, country, streetAddress, city, zipcode,
+                shipping, email) VALUES(:firstName, :lastName, :phone, :country, :streetAddress, :city, :zipcode,
+                                        :shipping, :email)";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(
+            ':firstName' ==> $_POST['firstName'],
+            ':lastName' ==> $_POST['lastName'],
+            ':phone' ==> $_POST['phone'],
+            ':country' ==> $_POST['country'],
+            ':streetAddress' ==> $_POST['streetAddress'],
+            ':city' ==> $_POST['city'],
+            ':zipcode' ==> $_POST['zipcode'],
+            ':shipping' ==> $_POST['shipping'],
+            ':email' ==> $_POST['email'],));
+
+    }
+
+    //CREDIT CARD INFO
+    if( isset($_POST['cardNumber']) && isset($_POST['expiration']) && isset($_POST['securityCode'])) {
+
+        $sql = "INSERT INTO creditcards VALUES(LAST_INSERT_ID(), :cardNumber, :expiration, :securityCode)";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(
+            ':cardNumber' ==> $_POST['cardNumber'],
+            ':expiration' ==> $_POST['expiration'],
+            ':securityCode' ==> $_POST['securityCode']
+    }
+
+    require('db_close.php');
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,62 +74,62 @@
         <form id="form-user" action="placedOrder.html">
             <div class="form-box error" data-errormsg="">
                 <label for="input-first">First Name</label>
-                <input type="text" id="input-first" autofocus required placeholder="First Name" tabindex="1"/>
+                <input type="text" id="input-first"  name="firstName" autofocus required placeholder="First Name" tabindex="1"/>
             </div>
             <div class="form-box error" data-errormsg="">
                 <label for="input-last">Last Name</label>
-                <input type="text" id="input-last" autofocus required placeholder="Last Name" tabindex="1"/>
+                <input type="text" id="input-last"  name="lastName" autofocus required placeholder="Last Name" tabindex="1"/>
             </div>
             <div class="form-box" data-errormsg="">
                 <label for="input-phone">Phone #</label>
-                <input type="text" id="input-phone" required placeholder="Example: 123-456-7890" tabindex="2" />
+                <input type="text" id="input-phone" required name="phone" placeholder="Example: 123-456-7890" tabindex="2" />
             </div>
             <div class="form-box" data-errormsg="">
                 <label for="input-slctCountry">Country</label>
-                <select id="input-slctCountry" tabindex="1">
-                    <option value="0">United States</option>
-                    <option value="1">Canada</option>
+                <select id="input-slctCountry" name="country" tabindex="1">
+                    <option value="United States">United States</option>
+                    <option value="Canada">Canada</option>
                 </select>
             </div>
             <div class="form-box error" data-errormsg="">
                 <label for="input-streetAddress">Street Adress</label>
-                <input type="text" id="input-streetAddress" autofocus required placeholder="Example: 1234 INF4MTX St" tabindex="1"/>
+                <input type="text" id="input-streetAddress" name="streetAddress" autofocus required placeholder="Example: 1234 INF4MTX St" tabindex="1"/>
             </div>
             <div class="form-box error" data-errormsg="">
                 <label for="input-city">City</label>
-                <input type="text" id="input-city" autofocus required placeholder="Example: Irvine" tabindex="1"/>
+                <input type="text" id="input-city" name="city" autofocus required placeholder="Example: Irvine" tabindex="1"/>
             </div>
             <div class="form-box error" data-errormsg="">
                 <label for="input-zipcode">Zipcode</label>
-                <input type="number" id="input-zipcode" autofocus required placeholder="Example: 12345" tabindex="1"/>
+                <input type="number" id="input-zipcode" name="zipcode" autofocus required placeholder="Example: 12345" tabindex="1"/>
             </div>
             <div class="form-box" data-errormsg="">
                 <label for="input-shipping">Shipping Method:</label>
-                <select id="input-shipping" tabindex="3">
-                    <option value="0">Anteater Express</option>
-                    <option value="1">2 Days Expedited</option>
-                    <option value="2">Over Weekend</option>
-                    <option value="3">7 Day Shipping</option>
+                <select id="input-shipping" name="shipping" tabindex="3">
+                    <option value="Anteater Express">Anteater Express</option>
+                    <option value="2 Days Expedited">2 Days Expedited</option>
+                    <option value="Over Weekend">Over Weekend</option>
+                    <option value="7 Day Shipping">7 Day Shipping</option>
                 </select>
             </div>
             <div class="form-box" data-errormsg="">
                 <label for="input-email">E-mail</label>
-                <input type="email" id="input-email" required placeholder="Example: petr@uci.edu" tabindex="3" />
+                <input type="email" id="input-email" name="email" required placeholder="Example: petr@uci.edu" tabindex="3" />
             </div>
 
             <!-- Credit Info Section -->
             <h1 id="cardinfo">Credit Card Information</h1>
             <div class="form-box error" data-errormsg="">
                 <label for="input-cardNumber">Card Number</label>
-                <input type="text" id="input-cardNumber" maxlength="19" autofocus required placeholder="Example: 1234 5678 9101 1234" tabindex="1"/>
+                <input type="text" id="input-cardNumber" name="cardNumber" maxlength="19" autofocus required placeholder="Example: 1234 5678 9101 1234" tabindex="1"/>
             </div>
             <div class="form-box error" data-errormsg="">
                 <label for="input-expiration">Expiration Date</label>
-                <input type="text" id="input-expiration" title="Format - Month/Full Year" autofocus required placeholder="Example: 04/2020" tabindex="1"/>
+                <input type="text" id="input-expiration" name="expiration" title="Format - Month/Full Year" autofocus required placeholder="Example: 04/2020" tabindex="1"/>
             </div>
             <div class="form-box error" data-errormsg="">
                 <label for="input-securityCode">Card Security Code</label>
-                <input type="number" id="input-securityCode" autofocus required placeholder="Example: 123" tabindex="1"/>
+                <input type="number" id="input-securityCode" name="securityCode" autofocus required placeholder="Example: 123" tabindex="1"/>
             </div>
             <div class="form-box">
                 <button id="button-cancel">Cancel</button>
